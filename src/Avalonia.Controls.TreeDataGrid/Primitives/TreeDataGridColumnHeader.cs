7c1c36f621e50fe997c9fec0f333801e36c4943a
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2025-01-09 13:02:16 +0100

    Add TreeDataGridColumnHeaderAutomationPeer

--- src/Avalonia.Controls.TreeDataGrid/Primitives/TreeDataGridColumnHeader.cs
+++ src/Avalonia.Controls.TreeDataGrid/Primitives/TreeDataGridColumnHeader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
+using Avalonia.Automation.Peers;
+using Avalonia.Controls.Automation.Peers;
 using Avalonia.Controls.Models.TreeDataGrid;
 using Avalonia.Controls.Utils;
 using Avalonia.Input;
@@ -79,6 +81,11 @@ namespace Avalonia.Controls.Primitives
             _model = null;
             ColumnIndex = -1;
             UpdatePropertiesFromModel();
+        }
+
+        protected override AutomationPeer OnCreateAutomationPeer()
+        {
+            return new TreeDataGridColumnHeaderAutomationPeer(this);
         }
 
         protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
